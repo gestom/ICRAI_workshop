2171b90da1389fde5b410c3295a7cfa245e2d39a
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2016-10-28 12:01:48 +0100

    Tutorial 1 skeleton files.

--- src/spot.cpp
+++ src/spot.cpp
@@ -5,16 +5,23 @@
 #include "image_transport/image_transport.h"
 #include <sstream>
 
-/**
- * Make the robot turn a predefined angle that might be larger than 2pi
- */
+//TASK 4: try to search for a green object in the robot's surrounding
+//HINT: count the number of green pixels in the image
+
+float currentYaw,lastYaw,initialYaw,turnVal;
+bool objectSpotted = false;
 
 //whenever we receive a message over the odom topic, this callback is activated
 void odomCallback(const nav_msgs::Odometry &msg)
 {
+	lastYaw = currentYaw;
 	//robot rotation is in quaternions, we just need to retreve its yaw
-	float yawAngle = tf::getYaw(msg.pose.pose.orientation);
-	ROS_INFO("Robot angle is: %f",yawAngle);
+	currentYaw = tf::getYaw(msg.pose.pose.orientation);
+
+	//calculate the total robot rotation
+	float diff = currentYaw-lastYaw;
+	if (diff > 0) turnVal+=diff;
+	ROS_INFO("Robot angle is: %f %f",currentYaw,turnVal);
 }
 
 //whenever we receive a message over the odom topic, this callback is activated
@@ -25,37 +32,27 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 	//msg->height = HEIGHT of the image in pixels
 	//msg->data[3*i+0] = red channel of ith pixel
 	//msg->data[3*i+1] = green channel of ith pixel
-	
-	unsigned char r,g,b;
-	int pixelCount = 0;
-	for (int cnt = 0; cnt<msg->width*msg->height;cnt++)
-	{
-		r = msg->data[cnt*3+0];
-		g = msg->data[cnt*3+1];
-		b = msg->data[cnt*3+2];
-		if (r > g + 100 && r > b +100) pixelCount++;
-	}
-	ROS_INFO("PXC: %i",pixelCount);
 }
 
 int main(int argc, char **argv)
 {
+	currentYaw=lastYaw=initialYaw=0;
 	ros::init(argc, argv, "turn");
 	ros::NodeHandle n;
 	ros::Subscriber subOdom = n.subscribe("odom", 1, odomCallback);
 	ros::Publisher commandRobot = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/navi", 1000);
-	/*special layer for image transport  */
+
+	/*special layer for image transport*/
 	image_transport::ImageTransport imageTransport(n);
 	image_transport::Subscriber subImage = imageTransport.subscribe("/camera/rgb/image_raw", 1, imageCallback);
 
 	ros::Rate loop_rate(10);
-	float angle = atof(argv[1]);
 
 	//form the twist command
 	geometry_msgs::Twist command;
 	command.angular.z = 0.2;
 
-	while (ros::ok())
+	while (ros::ok() && turnVal < 2*M_PI && objectSpotted == false)
 	{
 		commandRobot.publish(command);
 		ros::spinOnce();
