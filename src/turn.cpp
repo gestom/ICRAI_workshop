012b3e4f69b67bf16cc51c9b072000cba9afe476
Tom Krajnik <tkrajnik@lincoln.ac.uk>
2016-11-02 11:27:55 +0500

    Tutorial 3 solution

--- src/turn.cpp
+++ src/turn.cpp
@@ -8,10 +8,20 @@
 //HINT: subscribe to the relevant topic and use the robot's position callback to measure it's yaw  
 
 //whenever we receive a message over the odom topic, this callback is triggered 
+float lastAngle = -100;
+float currentAngle = -100;
+float turnVal = 0;
+
 void odomCallback(const nav_msgs::Odometry &msg)
 {
 	//robot rotation is in quaternions, we just need to retreve its yaw
-	ROS_INFO("Robot yaw: %f",tf::getYaw(msg.pose.pose.orientation));
+	currentAngle = tf::getYaw(msg.pose.pose.orientation);
+	if (lastAngle > -10){
+		float difference = currentAngle - lastAngle;	
+		if (difference > 0) turnVal += difference;	
+	}
+	lastAngle = tf::getYaw(msg.pose.pose.orientation);
+	ROS_INFO("Robot turn: %f",turnVal);
 }
 
 int main(int argc, char **argv)
@@ -22,13 +32,13 @@ int main(int argc, char **argv)
 	ros::Rate loop_rate(10);
 
 	//subscribing to the relevant topic
-	ros::Subscriber subOdom = n.subscribe("GUESS", 1, odomCallback);
+	ros::Subscriber subOdom = n.subscribe("/odom", 1, odomCallback);
 
 	//make the robot turn around
 	geometry_msgs::Twist command;
 	command.angular.z = 0.2;
 
-	while (ros::ok() /* && turn not completed */)
+	while (ros::ok() && turnVal < 6.28/* && turn not completed */)
 	{
 		commandRobot.publish(command);
 		ros::spinOnce();
